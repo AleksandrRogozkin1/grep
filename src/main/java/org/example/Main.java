a185eebbfadda7a4d07e42c3e09749cad84ce0e9
Aleksandr Rogozhkin <rogozkin371@gmail.com>
2023-07-03 23:13:03 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String result = "";
+        System.out.print("Введите текст:");
+        Scanner scanner1 = new Scanner(System.in);
+        String text = scanner1.nextLine();
+        System.out.print("Введите текст:");
+        Scanner scanner2 = new Scanner(System.in);
+        String word = scanner2.nextLine();
+// [.!?]\s*  цей вираз знайшов, сам не вмію писати
+        String sentences[] = text.split("[.!?]\\s*");
+        for(int i=0; i<sentences.length;i++){
+            if(sentences[i].contains(word)) result+=sentences[i]+"\n";
+        }
+        System.out.println(result);
+
+    }
+}

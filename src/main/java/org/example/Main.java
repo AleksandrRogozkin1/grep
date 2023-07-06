ddc73ebf11c39fb338a24679ade6a9f51b95d19b
Aleksandr Rogozhkin <rogozkin371@gmail.com>
2023-07-06 21:18:03 +0300

    innit 7

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,21 @@
 package org.example;
+
 import org.example.grepServices.GrepService;
 import org.example.utils.FileReader;
 
 import java.util.List;
 import java.util.Scanner;
 
-public class Main{
+public class Main {
 
     public static void main(String[] args) {
         System.out.print("Введите слово:");
         String word = new Scanner(System.in).nextLine();
 
-        List<String> res=new FileReader("text").reading();
-        System.out.println("Повний текст:"+res);
+        List<String> res = new FileReader("text").reading();
+        System.out.println("Повний текст:" + res);
 
-        String grepService=new GrepService().grepService(res,word);
+        String grepService = new GrepService().grepService(res, word);
         System.out.println(grepService);
     }
 }
--- src/main/java/org/example/utils/FileReader.java
+++ src/main/java/org/example/utils/FileReader.java
@@ -7,18 +7,18 @@ import java.util.Scanner;
 
 public class FileReader {
     private String sourceString;
-    private List<String> stringFromFile=new ArrayList<>();
+    private List<String> stringFromFile = new ArrayList<>();
 
-    public FileReader(String sourceString){
-        this.sourceString=sourceString;
+    public FileReader(String sourceString) {
+        this.sourceString = sourceString;
     }
 
-    public List<String> reading(){
-        if(sourceString!=null){
-            try(InputStream fis=new FileInputStream(sourceString);
-            Scanner scanner=new Scanner(fis)) {
-                while (scanner.hasNext()){
-                 stringFromFile.add(scanner.nextLine());
+    public List<String> reading() {
+        if (sourceString != null) {
+            try (InputStream fis = new FileInputStream(sourceString);
+                 Scanner scanner = new Scanner(fis)) {
+                while (scanner.hasNext()) {
+                    stringFromFile.add(scanner.nextLine());
                 }
             } catch (IOException e) {
                 throw new RuntimeException(e);
